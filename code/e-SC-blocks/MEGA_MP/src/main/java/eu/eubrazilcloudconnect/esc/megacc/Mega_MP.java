a49c39502bb5416947976125efa4fbf09d5e21d1
Jacek <jacek.cala@gmail.com>
2016-01-20 12:53:56 +0000

    Improved error handling in the Mega MP block. The fix following the previous changes in the ML and NJ blocks.

--- code/e-SC-blocks/MEGA_MP/src/main/java/eu/eubrazilcloudconnect/esc/megacc/Mega_MP.java
+++ code/e-SC-blocks/MEGA_MP/src/main/java/eu/eubrazilcloudconnect/esc/megacc/Mega_MP.java
@@ -1,5 +1,5 @@
 /**
- * e-Science Central Copyright (C) 2008-2015 School of Computing Science,
+ * e-Science Central Copyright (C) 2008-2016 School of Computing Science,
  * Newcastle University
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -15,6 +15,8 @@
  * this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, 5th Floor, Boston, MA 02110-1301, USA.
  */
+package eu.eubrazilcloudconnect.esc.megacc;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
@@ -36,6 +38,7 @@ public class Mega_MP implements WorkflowBlock
     private final static String Prop_INIT_TREES = "No. of Initial Trees (random addition)";
     private final static String Prop_SEARCH_LEVEL = "MP Search level";
     private final static String Prop_TREES_TO_RETAIN = "Max No. of Trees to Retain";
+    private final static String Prop_STOP_ON_ERROR = "Stop on error";
 
     private final static String Opt_MAXMIN_BNB_ESC = "Max-mini Branch-and-Bound";
     private final static String Opt_MAXMIN_BNB_MEGA = "Max-mini Branch-&-bound";
@@ -77,11 +80,22 @@ public class Mega_MP implements WorkflowBlock
         args.add("-o");
         args.add("<OUTPUT_FILE_PLACEHOLDER>");
 
-        execute_N_way(env, args, inputs.getInputFiles(Input_DATA), outputAnalyses, outputConsensus, outputSummaries);
+        try {
+            execute_N_way(args, inputs.getInputFiles(Input_DATA), env.getBooleanProperty(Prop_STOP_ON_ERROR, true), outputAnalyses, outputConsensus, outputSummaries);
 
-        outputs.setOutputFiles(Output_ANALYSIS, outputAnalyses);
-        outputs.setOutputFiles(Output_CONSENSUS, outputConsensus);
-        outputs.setOutputFiles(Output_SUMMARY, outputSummaries);
+            outputs.setOutputFiles(Output_SUMMARY, outputSummaries);
+            outputs.setOutputFiles(Output_ANALYSIS, outputAnalyses);
+            outputs.setOutputFiles(Output_CONSENSUS, outputConsensus);
+        } finally {
+            // Handle the summary output in the special case ...
+            if (outputSummaries.size() == 1) {
+                // when there is only one input/output file, print the summary to the block std output.
+                Files.copy(outputSummaries.get(0).toPath(), System.out);
+            } else {
+                // otherwise direct the user to inspect the summary files.
+                System.out.println("To see the detailed output of megacc run please inspect files from the " + Output_SUMMARY + " output port.");
+            }
+        }
     }
 
     /**
@@ -171,10 +185,11 @@ public class Mega_MP implements WorkflowBlock
     }
 
 
-    private void execute_N_way(BlockEnvironment env, ArrayList<String> args, List<File> inputFiles, List<File> outAnalysisFiles, List<File> outConsensusFiles, List<File> outSummaryFiles)
+    private void execute_N_way(ArrayList<String> args, List<File> inputFiles, boolean stopOnError, List<File> outAnalysisFiles, List<File> outConsensusFiles, List<File> outSummaryFiles)
     throws Exception
     {
         System.out.println("Running in the N-way mode");
+        boolean allErrors = true;
 
         for (File inputFile : inputFiles) {
             // Set the input file name
@@ -188,6 +203,13 @@ public class Mega_MP implements WorkflowBlock
 
             // Start a separate process for each input file.
             ProcessBuilder pb = new ProcessBuilder(args);
+
+            // A little hack to avoid problems with wine on VM created in UPV
+            Map<String, String> sysenv = pb.environment();
+            if (sysenv.get("TERM") == null || "unknown".equals(sysenv.get("TERM"))) {
+                System.out.println("Environment variable TERM not set correctly. Resetting to 'linux'");
+                sysenv.put("TERM", "linux");
+            }
             final Process child = pb.start();
 
             // Add a shutdown hook to kill the child in the case the block has been terminated.
@@ -213,15 +235,31 @@ public class Mega_MP implements WorkflowBlock
             stdErrTh.join();
 
             if (exitCode != 0) {
-                System.out.println("=====================================================");
+                System.out.println("Error processing file: " + inputFile);
                 System.out.println(getClass().getSimpleName() + " exited with code = " + exitCode);
-                throw new Exception(getClass().getSimpleName() + " failed; see output message for more details.");
+                //throw new Exception(getClass().getSimpleName() + " failed; see output message for more details.");
             }
 
             System.out.println("Adding: " + outputFile.toString() + ".nwk" + ", " + new File(outputFile.toString() + "_consensus.nwk") + ", " + new File(outputFile.toString() + "_summary.txt"));
             outAnalysisFiles.add(new File(outputFile.toString() + ".nwk"));
             outConsensusFiles.add(new File(outputFile.toString() + "_consensus.nwk"));
-            outSummaryFiles.add(new File(outputFile.toString() + "_summary.txt"));
+            File summaryFile = new File(outputFile.toString() + "_summary.txt");
+            outSummaryFiles.add(summaryFile);
+
+            String errorMsg = MegaCCTools.GetMegaCCErrorMessage(summaryFile);
+            if (errorMsg != null) {
+                if (stopOnError) {
+                    throw new MegaCCFailureException("MegaCC failed for file: " + inputFile + " with error: " + errorMsg);
+                } else {
+                    System.err.println("Error processing file: " + inputFile + ": " + errorMsg + "; inspect files from the " + Output_SUMMARY + " output port for more information.");
+                }
+            } else {
+                allErrors = false;
+            }
+        }
+
+        if (allErrors) {
+            throw new MegaCCFailureException("MegaCC failed for all input files!");
         }
     }
 }
